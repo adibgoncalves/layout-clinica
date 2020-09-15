61c10693096d2955e988676247519ff175f57614
Adib Gonçalves <adib.fernandes@gmail.com>
2020-09-15 18:11:02 -0300

    Subindo o layout

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    center: true,
+    items:1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsiveClass:true,
+    dots: true,
+    nav:false
+})
